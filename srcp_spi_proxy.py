cdfcfe1b186f3b2c38dd5365e56cbd66332f1b60
brederle <rbe-github@t-online.de>
2024-12-08 21:07:58 +0100

    Fix commandline parsing

--- srcp_spi_proxy.py
+++ srcp_spi_proxy.py
@@ -15,15 +15,14 @@ if __name__ == "__main__":
     parser.add_argument('--dev', type=int, default=0, help='SPI device id')
     parser.add_argument('reqport', type=int, help='Rocrail request port (4304)')
     parser.add_argument('srcpdport', type=int, help='Srcpd daemon port (4303)')
-    args = parser.parse_args(sys.argv)
+    args = parser.parse_args()
 
-    sensor1 = MCP23S17Sensor(bus=args['bus'], ce=args['ce'],
-                deviceID=args['dev'])
+    sensor1 = MCP23S17Sensor(bus=args.bus, ce=args.ce, deviceID=args.dev)
     
     # move srcp to the non-standard port
-    srcpSock = SRCPSock('localhost', args['reqport'])
+    srcpSock = SRCPSock('localhost', args.srcpdport)
     srcpSock.connect()
 
     # make proxy available on srcp standard port
-    proxy = RocrailProxy('localhost', args['srcpdport'], sensors=[ sensor1 ])
+    proxy = RocrailProxy('localhost', args.reqport, sensors=[ sensor1 ])
     proxy.forward(srcpSock)
